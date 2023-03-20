5e8b7b71ae1238a768fc13438e04cf41847c4d36
Tiela Black-Law <tiela.rose@gmail.com>
2023-03-19 19:52:31 -0700

    complete day1 part2 - calculate_sum_of_top_3_most_calories

--- day1.py
+++ day1.py
@@ -1,5 +1,3 @@
-import sys
-
 
 def calculate_most_calories(filepath):
     """given a file of calories carried by elves, return the greatest number of calories
@@ -39,4 +37,43 @@ def calculate_most_calories(filepath):
     return max_total
 
 
-# print(calculate_most_calories(sys.argv[1]))
+def calculate_sum_of_top_3_most_calories(filepath):
+    """given a file of calories carried by elves, return the sum of the top 3 most calories carried
+
+    input example:
+        1000
+        2000
+
+        3000
+
+        450
+        3000
+
+        2000
+
+        blank lines represent a new elf; in the example above:
+            elf 1: 3000
+            elf 2: 3000
+            elf 3: 3450
+            elf 4: 2000
+
+        this function would return the integer 9450 (3000 + 3000 + 3450)"""
+
+    file = open(filepath)
+
+    curr_total = 0
+    list_of_calories_carried = []
+
+    for line in file:
+        line = line.rstrip()
+
+        if line == '':
+            list_of_calories_carried.append(curr_total)
+            curr_total = 0
+            continue
+
+        curr_total += int(line)
+
+    list_of_calories_carried.sort()
+
+    return sum(list_of_calories_carried[-3:])
