bf6e7268b72a29a203072ec4b62ac955f232129f
Tiela Black-Law <tiela.rose@gmail.com>
2023-03-29 17:57:57 -0700

    Solve day3 part2

--- day3.py
+++ day3.py
@@ -40,8 +40,25 @@ def sum_priorities(filepath):
     return priorities_total
 
 
-def sum_common_keys(filepath):
-    # iterate over the file, get every 3 lines
+# part 2
+
+with open("day3_input.txt") as file:
+    # put each elf string into a list
+    elves = file.read().strip().split("\n")
+
+    # group the elves into 3
+    elf_groups = [elves[i:i+3] for i in range(0, len(elves), 3)]
+
     # find the common item in those 3 lines
+    badges = [(set(elf_group[0]) & set(elf_group[1]) & set(elf_group[2]))
+              for elf_group in elf_groups]
+
+    priorities_key = create_priorities_key()
+
     # convert that item to a priority/number
-    # add that to the total
+    priorities = [priorities_key[item.pop()] for item in badges]
+
+    print(f'Part 2 answer: {sum(priorities)}')
+
+[['vJrwpWtwJgWrhcsFMMfFFhFp', 'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL', 'PmmdzqPrVvPwwTWBwg'],
+    ['wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn', 'ttgJtRGJQctTZtZT', 'CrZsJsPPZsGzwwsLwLmpwMDw']]
